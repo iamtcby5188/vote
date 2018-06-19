fce3ffb7f67472a92df0fab426f0309600f4889a
yu.bian <yu.bian@sumscope.com>
2018-06-19 15:01:34 +0800

    sql add

---
+++ VoteServer/design/user_vote_info.sql
@@ -0,0 +1,31 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50720
+Source Host           : localhost:3306
+Source Database       : vote
+
+Target Server Type    : MYSQL
+Target Server Version : 50720
+File Encoding         : 65001
+
+Date: 2018-06-19 15:01:16
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user_vote_info
+-- ----------------------------
+DROP TABLE IF EXISTS `user_vote_info`;
+CREATE TABLE `user_vote_info` (
+  `id` varchar(32) NOT NULL,
+  `vote_id` varchar(32) DEFAULT NULL,
+  `vote_time` varchar(16) DEFAULT NULL,
+  `vote_topic` varchar(128) DEFAULT NULL,
+  `vote_description` varchar(255) DEFAULT NULL,
+  `user_id` varchar(32) DEFAULT NULL,
+  `dead_line` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ VoteServer/design/user_vote_option.sql
@@ -0,0 +1,28 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50720
+Source Host           : localhost:3306
+Source Database       : vote
+
+Target Server Type    : MYSQL
+Target Server Version : 50720
+File Encoding         : 65001
+
+Date: 2018-06-19 15:01:24
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user_vote_option
+-- ----------------------------
+DROP TABLE IF EXISTS `user_vote_option`;
+CREATE TABLE `user_vote_option` (
+  `id` varchar(32) NOT NULL,
+  `user_vote_id` varchar(32) DEFAULT NULL,
+  `vote_option_id` varchar(32) DEFAULT NULL,
+  `options` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
